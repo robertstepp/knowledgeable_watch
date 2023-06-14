c75cacc1fb8531e4ec2ecd1fb32cde5ff7169f0f
Robert Stepp <robert@vyperinc.ninja>
2023-06-13 18:54:35 -0700

    Update

--- timesheetprep.ps1
+++ timesheetprep.ps1
@@ -1,12 +1,15 @@
 <#
     Robert Stepp, robert@robertstepp.ninja
     Functionality -
-
 #>
 
 # Import the required .NET assemblies
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+# Global variables
+$startTime = $null
+$selectedButton = $null
 
 <# Debug settings
     No Debug output = SilentlyContinue
@@ -15,29 +18,18 @@
 $DebugPreference = 'Continue'
 
 # Start the transcript
-if ($DebugPreference -eq "Continue") {
-    # Create a new StringBuilder object
-    $logFileName = New-Object System.Text.StringBuilder
-    
-    # Append strings
-    [void]$logFileName.Append("DebugLogs\")
-    [void]$logFileName.Append((Get-Date -Format yyyyMMdd_HHmm))
-    [void]$logFileName.Append("_debug.log")
-    $logFile = Join-Path -Path (Get-ParentScriptFolder) -ChildPath $logFileName
-    Start-Transcript -Path $logFile -Append
-}
-Write-Debug "Debug Preference: $($DebugPreference)"
+$transcriptPath = "DebugLogs\" + (Get-Date -Format 'yyyyMMdd_HHmm') + "_debug.log"
+Start-Transcript -Path $transcriptPath -Append
 
 # Get the path to the parent folder
 function Get-ParentScriptFolder {
-    $scriptPath = $MyInvocation.PSCommandPath
-    $myParentFolder = Split-Path -Path $scriptPath
+    $thisScriptPath = $MyInvocation.PSCommandPath
+    $myParentFolder = Split-Path -Path $thisScriptPath
     Write-Debug "Parent Folder: $($myParentFolder)"
     return $myParentFolder
 }
 
-######################################
-
+# Read the config file
 function Read-ConfigFile {
     param (
         [Parameter(Mandatory = $true)]
@@ -71,6 +63,7 @@ function Read-ConfigFile {
     return $configData
 }
 
+# Show the item form
 function Show-ItemForm {
     param (
         [Parameter(Mandatory = $true)]
@@ -115,10 +108,12 @@ function Show-ItemForm {
             $selectedItems[$sender.Text] = $Items[$sender.Text]
             $sender.BackColor = [System.Drawing.Color]::White
             $sender.ForeColor = [System.Drawing.Color]::Red
+            Write-Debug $sender
+            Save-ButtonPress -ButtonText $sender.Text
         }
 
         # Assign the closure as the button's event handler
-        $button.Add_Click({ $handler.Invoke($this) })
+        $button.Add_Click($handler)
 
         # Add the button to the form
         $form.Controls.Add($button)
@@ -134,6 +129,10 @@ function Show-ItemForm {
     $doneButton.BackColor = [System.Drawing.Color]::Gray
     $doneButton.Add_Click({
         $form.Close()
+        Export-Timesheet
+        # Delete the raw_timesheet CSV file
+        Remove-Item -Path "raw_timesheet.csv" -Force -ErrorAction SilentlyContinue
+    
     })
 
     # Add the "Done" button to the form
@@ -145,29 +144,154 @@ function Show-ItemForm {
         $_.ForeColor = [System.Drawing.Color]::Black
     }
 
-    # Show the form
+    # Show the form on top of all windows
+    $form.TopMost = $true
     [void]$form.ShowDialog()
-
-    # Return the selected items
-    return $selectedItem
 }
 
-# Example usage:
+# Save the button press information
+function Save-ButtonPress {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ButtonText
+    )
+
+    # Check if the button is the currently selected button
+    if ($ButtonText -eq $global:selectedButton) {
+        Write-Debug "Button already selected."
+        return
+    }
+
+    # Check if the start time is already set
+    if ($null -eq $global:startTime) {
+        $global:startTime = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+        Write-Debug "Start time: $($global:startTime)"
+    } else {
+        # Get the end time and calculate the duration
+        $endTime = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+        $duration = New-TimeSpan -Start $global:startTime -End $endTime
+        Write-Debug "End time: $endTime"
+        Write-Debug "Duration: $($duration.TotalSeconds) seconds"
+
+        # Write the data to the CSV file
+        $data = [PSCustomObject]@{
+            Button = $ButtonText
+            Start = $global:startTime
+            End = $endTime
+            Duration = $duration.TotalSeconds
+        }
+        $data | Export-Csv -Path "raw_timesheet.csv" -Append -NoTypeInformation
+
+        # Store the selected button
+        $global:selectedButton = $ButtonText
+
+        Write-Debug "Button selected: $ButtonText"
+        $global:startTime = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+        Write-Debug "Start time: $($global:startTime)"
+    }
+}
+
+# Generate the timesheet
+function Export-Timesheet {
+    # Read the raw_timesheet CSV file
+    $rawTimesheet = Import-Csv -Path "raw_timesheet.csv"
+
+    # Group the raw timesheet data by button and calculate the total duration
+    $groupedTimesheet = $rawTimesheet |
+        Group-Object -Property Button |
+        Select-Object @{Name = 'Button'; Expression = {$_.Name}}, @{Name = 'TotalDuration'; Expression = {($_.Group | Measure-Object -Property Duration -Sum).Sum}}
+
+    # Create a new Excel workbook
+    $excel = New-Object -ComObject Excel.Application
+    $workbook = $excel.Workbooks.Add()
+    $worksheet = $workbook.Worksheets.Add()
+
+    # Set the worksheet name to "Today's Time"
+    $date = Get-Date -Format 'yyyyMMdd'
+    $worksheet.Name = "Today's Time"
+
+    # Write the timesheet data to the worksheet
+    $worksheet.Cells.Item(1, 1) = "Button"
+    $worksheet.Cells.Item(1, 2) = "Total Duration (Minutes)"
+    $worksheet.Cells.Item(1, 3) = "Total Duration (Tenths per Hour)"
+    $row = 2
+    foreach ($item in $groupedTimesheet) {
+        $durationMinutes = [Math]::Ceiling($item.TotalDuration / 60)
+        $durationTenthsPerHour = [Math]::Ceiling(($item.TotalDuration / 60) * 10)
+        $worksheet.Cells.Item($row, 1) = $item.Button
+        $worksheet.Cells.Item($row, 2) = $durationMinutes
+        $worksheet.Cells.Item($row, 3) = $durationTenthsPerHour
+        $row++
+    }
+
+    # Save the workbook to an Excel file
+    $scriptDirectory = (Get-ParentScriptFolder) 
+    $excelFile = Join-Path -Path $scriptDirectory -ChildPath "Timesheet-$date.xlsx"
+    $workbook.SaveAs($excelFile)
+
+    Write-Host "Timesheet generated: $excelFile"
+
+    # Remove the "Sheet1" from the workbook, if it exists
+    $sheetName = "Sheet1"
+    $sheet = $workbook.Sheets | Where-Object { $_.Name -eq $sheetName }
+    if ($sheet) {
+        $sheet.Delete()
+    }
+
+    # Add a new sheet for the raw timesheet data
+    $rawTimesheetSheet = $workbook.Worksheets.Add()
+    $rawTimesheetSheet.Name = "raw-timesheet"
+
+    # Write the raw timesheet data to the worksheet
+    $row = 1
+    foreach ($item in $rawTimesheet) {
+        $column = 1
+        foreach ($property in $item.PSObject.Properties) {
+            $rawTimesheetSheet.Cells.Item($row, $column) = $property.Value
+            $column++
+        }
+        $row++
+    }
+
+    # Reorder the sheets in the workbook
+    $worksheet = $workbook.Worksheets | Where-Object { $_.Name -eq "Today's Time" }
+    $worksheet.Move($workbook.Sheets.Item(1))
+
+    # Save the workbook to include the raw timesheet data
+    $workbook.Save()
+
+    # Close the workbook and Excel application
+    $workbook.Close()
+    $excel.Quit()
+
+    # Delete the raw_timesheet CSV file
+    Remove-Item -Path "raw_timesheet.csv" -Force -Confirm:$false -ErrorAction SilentlyContinue
+}
+
+function Test-ExistingTimesheet {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ExcelFile
+    )
+
+    # Check if the Excel file exists
+    if (Test-Path $ExcelFile) {
+        # Prompt the user for confirmation to proceed
+        $result = [System.Windows.Forms.MessageBox]::Show("An Excel spreadsheet for today already exists. Do you want to continue and overwrite it?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+        if ($result -ne [System.Windows.Forms.DialogResult]::Yes) {
+            Write-Host "Operation canceled. Script will exit."
+            exit
+        }
+    }
+}
+
+$scriptDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+$excelFile = Join-Path -Path $scriptDirectory -ChildPath "Timesheet-$(Get-Date -Format 'yyyyMMdd').xlsx"
+Test-ExistingTimesheet -ExcelFile $excelFile
+
 $configFile = "config.ini"
-
-Write-Debug "Reading config file..."
 $configData = Read-ConfigFile -FilePath $configFile
+Show-ItemForm -Items $configData
 
-Write-Debug "Showing item form..."
-$selectedItems = Show-ItemForm -Items $configData
-
-# Calculate totals or perform further operations with the selected items
-Write-Debug "Selected items:"
-Write-Host $selectedItems
-
-######################################
-
-if ($DebugPreference -eq "Continue") {
-    # Stop the transcript
-    Stop-Transcript
-}
+# Stop the transcript
+Stop-Transcript
